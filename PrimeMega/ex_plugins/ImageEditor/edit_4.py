b4aa79fa71154cb77dfc8a7b7061bf903fae8026
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-11-29 14:25:27 +0700

    Update edit_4.py

--- PrimeMega/ex_plugins/ImageEditor/edit_4.py
+++ PrimeMega/ex_plugins/ImageEditor/edit_4.py
@@ -8,7 +8,7 @@ import numpy as np
 import requests
 from PIL import Image, ImageDraw, ImageOps
 
-from SkyzuRobot.confing import get_str_key
+from PrimeMega.confing import get_str_key
 
 RemoveBG_API = get_str_key("REM_BG_API_KEY", required=False)
 
