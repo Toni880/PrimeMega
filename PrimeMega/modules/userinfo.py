79d086f26b28dced16232c0c928e17d23c49f83c
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-11-29 14:17:55 +0700

    Update userinfo.py

--- PrimeMega/modules/userinfo.py
+++ PrimeMega/modules/userinfo.py
@@ -31,7 +31,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
     
-from PrimeMe import (
+from PrimeMega import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
