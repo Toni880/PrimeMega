d7ec48f9c7ad1e5134f2752e99c4cfcd394f8ebf
Xbarox <92025643+Xbaroxx@users.noreply.github.com>
2021-11-29 19:51:27 +0700

    Update misc.py

--- PrimeMega/modules/helper_funcs/misc.py
+++ PrimeMega/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Go Home", callback_data="Prime_back")]]
+        pairs += [[EqInlineKeyboardButton("Go Home", callback_data="aboutmanu_back")]]
 
     return pairs
 
