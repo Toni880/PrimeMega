58204dfa9e4fd2ecb698d3943458202eb5ddff22
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-11-29 10:12:10 +0700

    Create apakah.py

---
+++ PrimeMega/modules/apakah.py
@@ -0,0 +1,26 @@
+import random
+from PrimeMega.events import register
+from PrimeMega import telethn
+
+APAKAH_STRING = ["Iya", 
+                 "Tidak", 
+                 "Mungkin", 
+                 "Mungkin Tidak", 
+                 "Bisa jadi", 
+                 "Mungkin Tidak",
+                 "Tidak Mungkin",
+                 "YNTKTS",
+                 "Pala bapak kau pecah",
+                 "Apa iya?",
+                 "Tanya aja sama mamak kau tu pler",
+                 "Lu tanya gua, terus gua tanya siapa?"
+                 ]
+
+
+@register(pattern="^/apakah ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply('Berikan saya pertanyaan 😐')
+        return
+    await event.reply(random.choice(APAKAH_STRING))
