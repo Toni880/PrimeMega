f6b630c815f8e3451eaf124c640bd430051c036c
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-06 20:23:41 +0700

    Update feds_sql.py

--- PrimeMega/modules/sql/feds_sql.py
+++ PrimeMega/modules/sql/feds_sql.py
@@ -10,6 +10,7 @@ from sqlalchemy import (
   String,
   UnicodeText,
 )
+from sqlalchemy.sql.sqltypes import BigInteger
 from telegram.error import BadRequest, Unauthorized
 
 
@@ -65,7 +66,7 @@ class BansF(BASE):
 
 class FedsUserSettings(BASE):
     __tablename__ = "feds_settings"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     should_report = Column(Boolean, default=True)
 
     def __init__(self, user_id):
