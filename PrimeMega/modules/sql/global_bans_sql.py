fbfc8b08a8b1db336951f81a11a219fe0e647191
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-06 20:25:11 +0700

    Update global_bans_sql.py

--- PrimeMega/modules/sql/global_bans_sql.py
+++ PrimeMega/modules/sql/global_bans_sql.py
@@ -8,11 +8,12 @@ from sqlalchemy import (
   String,
   UnicodeText,
 )
+from sqlalchemy.sql.sqltypes import BigInteger
 
 
 class GloballyBannedUsers(BASE):
     __tablename__ = "gbans"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     name = Column(UnicodeText, nullable=False)
     reason = Column(UnicodeText)
 
