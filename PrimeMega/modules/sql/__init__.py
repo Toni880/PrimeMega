9af5420eb7b2ed6adac5ce92f7a491e5bbe89e96
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-22 22:05:20 +0700

    ok

--- PrimeMega/modules/sql/__init__.py
+++ PrimeMega/modules/sql/__init__.py
@@ -1,15 +1,25 @@
-from PrimeMega import DB_URI
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from PrimeMega import DB_URL, LOGGER
+
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    LOGGER.info("PostgreSQL Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+    return scoped_session(sessionmaker(bind=engine, autoflush=True))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    LOGGER.exception(f"PostgreSQL Failed to connect due to {e}")
+    sys.exit()
+
+LOGGER.info("PostgreSQL Connection successful, session started.")
