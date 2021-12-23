18433f4419679e417e693d9699ecbe560a6b0eca
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-23 21:37:43 +0700

    Update __init__.py

--- PrimeMega/modules/sql/__init__.py
+++ PrimeMega/modules/sql/__init__.py
@@ -2,17 +2,17 @@ import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-from PrimeMega import DB_URL, LOGGER
+from PrimeMega import DATABASE_URL, LOGGER
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URL, client_encoding="utf8")
+    engine = create_engine(DATABASE_URL, client_encoding="utf8")
     LOGGER.info("PostgreSQL Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
