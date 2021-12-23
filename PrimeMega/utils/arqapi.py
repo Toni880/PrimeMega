4982c90b04c9542a109d72d2a0c7a570986585de
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-23 21:22:11 +0700

    Update arqapi.py

--- PrimeMega/utils/arqapi.py
+++ PrimeMega/utils/arqapi.py
@@ -4,19 +4,14 @@ from random import randint
 from time import time
 
 import aiohttp
-from PrimeMega import aiohttpsession 
+from PrimeMega import aiohttpsession
 from aiohttp import ClientSession
 
 from google_trans_new import google_translator
 from Python_ARQ import ARQ
 from search_engine_parser import GoogleSearch
 
-from PrimeMega import (
-  BOT_ID,
-  OWNER_ID,
-  ARQ_API_URL,
-  ARQ_API_KEY,
-)
+from PrimeMega import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
 from PrimeMega import pbot
 
 ARQ_API = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
