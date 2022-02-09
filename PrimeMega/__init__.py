c53c7d3040739ccb905cb82b2fe5f4665929d7fe
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-02-09 12:52:16 +0700

    Update __init__.py

--- PrimeMega/__init__.py
+++ PrimeMega/__init__.py
@@ -85,6 +85,7 @@ if ENV:
 
     INFOPIC = bool(os.environ.get("INFOPIC", True))
     BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+    BOT_NAME = os.environ.get("BOT_NAME", None)
     EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
     URL = os.environ.get("URL", "")  # Does not contain token
@@ -201,6 +202,7 @@ else:
     SESSION_STRING = Config.SESSION_STRING
     INFOPIC = Config.INFOPIC
     BOT_USERNAME = Config.BOT_USERNAME
+    BOT_NAME = config.BOT_NAME
     STRING_SESSION = Config.STRING_SESSION
     LASTFM_API_KEY = Config.LASTFM_API_KEY
     CF_API_KEY = Config.CF_API_KEY
