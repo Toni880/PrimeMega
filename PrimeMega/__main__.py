61bfb730433ab62aa43766ee0fc8e81475af13b6
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-16 14:21:15 +0700

    Update __main__.py

--- PrimeMega/__main__.py
+++ PrimeMega/__main__.py
@@ -83,8 +83,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = f"""
- ───「["{BOT_NAME}"]("https://t.me/{BOT_USERNAME}")」───
+PM_START_TEXT = """
 *Hello {} !*
 ────────────────────────
 PrimeMega a powerful group management bot built to help you manage your group!
