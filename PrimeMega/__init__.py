7656b23af4c5d7e12098b84714d8e2eba642c2a4
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-27 10:26:54 +0700

    Lupa bg

--- PrimeMega/__init__.py
+++ PrimeMega/__init__.py
@@ -125,7 +125,7 @@ if ENV:
     CF_API_KEY = os.environ.get("CF_API_KEY", None)
     WELCOME_DELAY_KICK_SEC = os.environ.get("WELCOME_DELAY_KICL_SEC", None)
     BOT_ID = int(os.environ.get("BOT_ID", None))
-    ARQ_API_URL = os.environ.get("ARQ_API_URL", None)
+    ARQ_API_URL = os.environ.get("ARQ_API_URL", "https://arq.hamker.in")
     ARQ_API_KEY = os.environ.get("ARQ_API_KEY", None)
     ERROR_LOGS = os.environ.get("ERROR_LOGS", None)
     PHOTO = os.environ.get("PHOTO", "https://telegra.ph/file/efc27dec817626cc95016.jpg")
