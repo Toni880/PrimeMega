f639510c0e4e13a52726c6081bb0f66ca2bf3d93
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-04-06 20:28:03 +0700

    Update eror_handler.py

--- PrimeMega/modules/eror_handler.py
+++ PrimeMega/modules/eror_handler.py
@@ -1,22 +1,26 @@
-import traceback
-import requests
 import html
+import sys
+import traceback
 import random
-
+import pretty_errors
+import requests
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
 
-from PrimeMega import (
-  dispatcher,
-  DEV_USERS,
-  OWNER_ID,
-)
+from PrimeMega import dispatcher, ERROR_LOGS, DEV_USERS
+
+pretty_errors.mono()
 
 
 class ErrorsDict(dict):
-    "A custom dict to store errors and their count"
+    """A custom dict to store errors and their count"""
+
+    def __init__(self, *args, **kwargs):
+        self.raw = []
+        super().__init__(*args, **kwargs)
 
     def __contains__(self, error):
+        self.raw.append(error)
         error.identifier = "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=5))
         for e in self:
             if type(e) is type(error) and e.args == error.args:
@@ -25,6 +29,9 @@ class ErrorsDict(dict):
         self[error] = 0
         return False
 
+    def __len__(self):
+        return len(self.raw)
+
 
 errors = ErrorsDict()
 
@@ -32,49 +39,71 @@ errors = ErrorsDict()
 def error_callback(update: Update, context: CallbackContext):
     if not update:
         return
-    if context.error in errors:
-        return
-    tb_list = traceback.format_exception(
-        None, context.error, context.error.__traceback__
-    )
-    tb = "".join(tb_list)
-    pretty_message = (
-        "An exception was raised while handling an update\n"
-        "User: {}\n"
-        "Chat: {} {}\n"
-        "Callback data: {}\n"
-        "Message: {}\n\n"
-        "Full Traceback: {}"
-    ).format(
-        update.effective_user.id,
-        update.effective_chat.title if update.effective_chat else "",
-        update.effective_chat.id if update.effective_chat else "",
-        update.callback_query.data if update.callback_query else "None",
-        update.effective_message.text if update.effective_message else "No message",
-        tb,
-    )
-    key = requests.post(
-        "https://nekobin.com/api/documents", json={"content": pretty_message}
-    ).json()
-    e = html.escape(f"{context.error}")
-    if not key.get("result", {}).get("key"):
-        with open("error.txt", "w+") as f:
-            f.write(pretty_message)
-        context.bot.send_document(
-            OWNER_ID,
-            open("error.txt", "rb"),
-            caption=f"#{context.error.identifier}\n<b>Your sugar mommy got an error for you, you cute guy:</b>\n<code>{e}</code>",
+    if context.error not in errors:
+        try:
+            stringio = io.StringIO()
+            pretty_errors.output_stderr = stringio
+            output = pretty_errors.excepthook(
+                type(context.error),
+                context.error,
+                context.error.__traceback__,
+            )
+            pretty_errors.output_stderr = sys.stderr
+            pretty_error = stringio.getvalue()
+            stringio.close()
+        except:
+            pretty_error = "Failed to create pretty error."
+        tb_list = traceback.format_exception(
+            None,
+            context.error,
+            context.error.__traceback__,
+        )
+        tb = "".join(tb_list)
+        pretty_message = (
+            "{}\n"
+            "-------------------------------------------------------------------------------\n"
+            "An exception was raised while handling an update\n"
+            "User: {}\n"
+            "Chat: {} {}\n"
+            "Callback data: {}\n"
+            "Message: {}\n\n"
+            "Full Traceback: {}"
+        ).format(
+            pretty_error,
+            update.effective_user.id,
+            update.effective_chat.title if update.effective_chat else "",
+            update.effective_chat.id if update.effective_chat else "",
+            update.callback_query.data if update.callback_query else "None",
+            update.effective_message.text if update.effective_message else "No message",
+            tb,
+        )
+        key = requests.post(
+            "https://www.toptal.com/developers/hastebin/documents",
+            data=pretty_message.encode("UTF-8"),
+        ).json()
+        e = html.escape(f"{context.error}")
+        if not key.get("key"):
+            with open("error.txt", "w+") as f:
+                f.write(pretty_message)
+            context.bot.send_document(
+                ERROR_LOGS,
+                open("error.txt", "rb"),
+                caption=f"#{context.error.identifier}\n<b>I have a error for you sir, please fix that:"
+                f"</b>\n<code>{e}</code>",
+                parse_mode="html",
+            )
+            return
+        key = key.get("key")
+        url = f"https://www.toptal.com/developers/hastebin/{key}"
+        context.bot.send_message(
+            ERROR_LOGS,
+            text=f"#{context.error.identifier}\n<b>I have a error for you sir, please fix that:"
+            f"</b>\n<code>{e}</code>",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("See Errors", url=url)]],
+            ),
             parse_mode="html",
         )
-        return
-    key = key.get("result").get("key")
-    url = f"https://nekobin.com/{key}.py"
-    context.bot.send_message(
-        OWNER_ID,
-        text=f"#{context.error.identifier}\n<b>Your sugar mommy got an error for you, you cute guy:</b>\n<code>{e}</code>",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Nekobin", url=url)]]),
-        parse_mode="html",
-    )
 
 
 def list_errors(update: Update, context: CallbackContext):
@@ -82,8 +111,19 @@ def list_errors(update: Update, context: CallbackContext):
         return
     e = dict(sorted(errors.items(), key=lambda item: item[1], reverse=True))
     msg = "<b>Errors List:</b>\n"
-    for x in e:
-        msg += f"• <code>{x}:</code> <b>{e[x]}</b> #{x.identifier}\n"
+    for x, value in e.items():
+        msg += f"• <code>{x}:</code> <b>{value}</b> #{x.identifier}\n"
+    msg += f"{len(errors)} have occurred, please fix it."
+    if len(msg) > 4096:
+        with open("errors_msg.txt", "w+") as f:
+            f.write(msg)
+        context.bot.send_document(
+            update.effective_chat.id,
+            open("errors_msg.txt", "rb"),
+            caption="Too many errors have occured..",
+            parse_mode="html",
+        )
+        return
     update.effective_message.reply_text(msg, parse_mode="html")
 
 
